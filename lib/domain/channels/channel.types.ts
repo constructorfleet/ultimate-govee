4328f657b3fb36acd2e0c4fc60ae0094e3711a48
GitHub Action <action@github.com>
2024-02-19 13:36:29 -0700

    Channels module configuration

--- lib/domain/channels/channel.types.ts
+++ lib/domain/channels/channel.types.ts
@@ -1,8 +1,11 @@
+import { BehaviorSubject } from 'rxjs';
 import { BleChannelService } from './ble/ble-channel.service';
 import { BleChannelModuleOptions } from './ble/ble-channel.types';
 import { IoTChannelService } from './iot/iot-channel.service';
 import { IoTChannelModuleOptions } from './iot/iot-channel.types';
 import { RestChannelService } from './rest/rest-channel.service';
+import { RestChannelModuleOptions } from './rest/rest-channel.types';
+import { ConfigurableModuleBuilder } from '@nestjs/common';
 
 export type TogglableChannels = Extract<
   BleChannelService | IoTChannelService | RestChannelService,
@@ -13,8 +16,33 @@ export type ChannelToggle = {
   [S in TogglableChannels as S['name']]: Pick<S, 'setConfig' | 'setEnabled'>;
 };
 
+export type ChannelState<TConfig extends object> = {
+  config: BehaviorSubject<TConfig | undefined>;
+  enabled: BehaviorSubject<boolean | undefined>;
+};
+
+export type Togglable = {
+  togglable: boolean;
+};
+
 export type ChannelModuleOptions = {
-  ble: BleChannelModuleOptions,
-  iot: IoTChannelModuleOptions,
-  rest:
-}
+  ble: BleChannelModuleOptions;
+  iot: IoTChannelModuleOptions;
+  rest: RestChannelModuleOptions;
+};
+
+export const {
+  ConfigurableModuleClass,
+  ASYNC_OPTIONS_TYPE,
+  OPTIONS_TYPE,
+  MODULE_OPTIONS_TOKEN,
+} = new ConfigurableModuleBuilder<ChannelModuleOptions>({
+  moduleName: 'ChannelModule',
+  optionsInjectionToken: 'Channel.Module.Options',
+})
+  .setExtras({ isGlobal: true }, (definition, extras) => ({
+    ...definition,
+    global: extras.isGlobal,
+  }))
+  .setClassMethodName('forRoot')
+  .build();
