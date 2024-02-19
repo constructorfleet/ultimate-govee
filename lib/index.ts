4ac35395affc9d568615133b836d49fab0949321
GitHub Action <action@github.com>
2024-02-18 17:55:08 -0700

    Export device

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
+import { Device } from './domain/devices/device';
 export * from './ultimate-govee.module';
 export * from './ultimate-govee.service';
 export * from './ultimate-govee.config';
 export { Devices, DeviceStates } from './domain';
+export { Device };
