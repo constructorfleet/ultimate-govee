44f01d887242e2ab7879c10db17e556d1a443ded
GitHub Action <action@github.com>
2024-02-18 21:48:10 -0700

    State names

--- lib/domain/devices/index.ts
+++ lib/domain/devices/index.ts
@@ -10,21 +10,48 @@ import { AirQualitySensor } from './impl/home-improvement/air-quality/air-qualit
 import { HygrometerSensor } from './impl/home-improvement/hygrometer/hygrometer';
 import { RGBICLight } from './impl/lights/rgbic/rgbic-light';
 import { RGBLight } from './impl/lights/rgb/rgb-light';
-import { ActiveState } from './states/active.state';
-import { BatteryLevelState } from './states/battery-level.state';
-import { BrightnessState } from './states/brightness.state';
-import { ColorRGBState } from './states/color-rgb.state';
-import { ColorTempState } from './states/color-temp.state';
-import { ControlLockState } from './states/control-lock.state';
-import { DisplayScheduleState } from './states/display-schedule.state';
-import { FilterState } from './states/filter.state';
-import { HumidityState } from './impl/home-improvement/air-quality/air-quality.humidity';
-import { LightEffectState } from './states/light-effect.state';
-import { NightLightState } from './states/night-light.state';
-import { PowerState } from './states/power.state';
-import { SegmentCountState } from './states/segment-count.state';
-import { TemperatureState } from './impl/home-improvement/air-quality/air-quality.temperature';
-import { WaterShortageState } from './states/water-shortage.state';
+import { ActiveState, ActiveStateName } from './states/active.state';
+import {
+  BatteryLevelState,
+  BatteryLevelStateName,
+} from './states/battery-level.state';
+import {
+  BrightnessState,
+  BrightnessStateName,
+} from './states/brightness.state';
+import { ColorRGBState, ColorRGBStateName } from './states/color-rgb.state';
+import { ColorTempState, ColorTempStateName } from './states/color-temp.state';
+import {
+  ControlLockState,
+  ControlLockStateName,
+} from './states/control-lock.state';
+import {
+  DisplayScheduleState,
+  DisplayScheduleStateName,
+} from './states/display-schedule.state';
+import { FilterState, FilterStateName } from './states/filter.state';
+import { HumidityState, HumidityStateName } from './states';
+import {
+  LightEffectState,
+  LightEffectStateName,
+} from './states/light-effect.state';
+import {
+  NightLightState,
+  NightLightStateName,
+} from './states/night-light.state';
+import { PowerState, PowerStateName } from './states/power.state';
+import {
+  SegmentCountState,
+  SegmentCountStateName,
+} from './states/segment-count.state';
+import {
+  TemperatureState,
+  TemperatureStateName,
+} from './states/temperature.state';
+import {
+  WaterShortageState,
+  WaterShortageStateName,
+} from './states/water-shortage.state';
 
 export * from './devices.module';
 export * from './devices.service';
@@ -39,20 +66,35 @@ export {
   RGBICLight,
   RGBLight,
   ActiveState,
+  ActiveStateName,
   BatteryLevelState,
+  BatteryLevelStateName,
   BrightnessState,
+  BrightnessStateName,
   ColorRGBState,
+  ColorRGBStateName,
   ColorTempState,
+  ColorTempStateName,
   ControlLockState,
+  ControlLockStateName,
   DisplayScheduleState,
+  DisplayScheduleStateName,
   FilterState,
+  FilterStateName,
   HumidityState,
+  HumidityStateName,
   LightEffectState,
+  LightEffectStateName,
   NightLightState,
+  NightLightStateName,
   PowerState,
+  PowerStateName,
   SegmentCountState,
+  SegmentCountStateName,
   TemperatureState,
+  TemperatureStateName,
   WaterShortageState,
+  WaterShortageStateName,
 };
 
 export const CQRS = cqrs;
