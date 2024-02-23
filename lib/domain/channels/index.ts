72b178067277900553cb6b570fdcabaccbda897b
GitHub Action <action@github.com>
2024-02-23 00:26:51 -0700

    fix Exports

--- lib/domain/channels/index.ts
+++ lib/domain/channels/index.ts
@@ -1,8 +1,14 @@
+import { InjectChannels } from './channel.providers';
 export * from './iot';
 export * from './ble';
 export * from './rest';
 export * from './channels.module';
-import { OPTIONS_TYPE, ASYNC_OPTIONS_TYPE } from './channel.types';
+import {
+  OPTIONS_TYPE,
+  ASYNC_OPTIONS_TYPE,
+  ChannelToggle,
+} from './channel.types';
 
 export const ChannelModuleOptions = typeof OPTIONS_TYPE;
 export const AsyncChannelModuleOptions = typeof ASYNC_OPTIONS_TYPE;
+export { ChannelToggle, InjectChannels };
