e4f0c7fff58b9a21656fa234f797c34ab7dd80e2
Teagan Glenn <that@teagantotally.rocks>
2024-03-04 02:27:45 -0700

    chore: Export enum, emit light effect change (#83)
    * Export light effect
    
    * Remove accidental paste
    
    * Emit event when effects changed

--- lib/domain/devices/impl/lights/light.device.ts
+++ lib/domain/devices/impl/lights/light.device.ts
@@ -1,6 +1,28 @@
-import { Device } from '../../device';
+import { Device, StateFactories } from '../../device';
 import { DeviceStatesType } from '../../devices.types';
+import { DeviceModel } from '../../devices.model';
+import { EventBus, CommandBus } from '@nestjs/cqrs';
+import {
+  LightEffectState,
+  LightEffectStateName,
+} from '../../states/light-effect.state';
 
 export abstract class LightDevice<
   States extends DeviceStatesType,
-> extends Device<States> {}
+> extends Device<States> {
+  constructor(
+    device: DeviceModel,
+    eventbus: EventBus,
+    commandBus: CommandBus,
+    stateFactories: StateFactories,
+  ) {
+    super(device, eventbus, commandBus, stateFactories);
+    const effectState = this.state<LightEffectState>(LightEffectStateName);
+    if (effectState === undefined) {
+      return;
+    }
+    effectState.effects.delta$.subscribe(() => {
+      this.next(this);
+    });
+  }
+}
