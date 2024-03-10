1733bd53df0305561c1705e428925df503877999
Teagan42 <that@teagantotally.rocks>
2024-03-10 00:22:04 -0700

    Missing files

---
+++ lib/domain/devices/impl/home-improvement/presence/presence.states.spec.ts
@@ -0,0 +1,405 @@
+import { DeviceModel } from '../../../devices.model';
+import { Version } from '../../../version.info';
+import {
+  BiologicalPresenceState,
+  EnablePresenceFlags,
+  EnablePresenceState,
+  MMWavePresenceState,
+} from './presence.states';
+import { OpType, asOpCode } from '../../../../../common';
+import {
+  testParseStateCalled,
+  testParseStateNotCalled,
+  testSetStateCalled,
+  testSetStateNotCalled,
+} from '../../../../../common/test-utils';
+
+const deviceModel: DeviceModel = new DeviceModel({
+  id: '01:02:03:04:05:06:07:08',
+  ic: 0,
+  name: 'Test device',
+  category: 'Home Improvement',
+  categoryGroup: 'Sensors',
+  model: 'H7567',
+  modelName: 'Presence Sensor',
+  goodsType: 30,
+  pactType: 10,
+  pactCode: 4,
+  version: new Version('1.0.0', '2.0.0'),
+  state: {},
+});
+
+describe('MMWavePresenceState', () => {
+  let state: MMWavePresenceState;
+
+  describe('parse', () => {
+    beforeEach(() => {
+      state = new MMWavePresenceState(deviceModel);
+    });
+
+    describe('when passed', () => {
+      describe('an invalid', () => {
+        describe('state argument', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {}),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('op codes with different OpType', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {
+                op: { command: [[0x02, 0x02, 0x01]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('op codes with different identifier', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {
+                op: { command: [[OpType.REPORT, 0x02, 0x01]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+      });
+    });
+    describe('a valid', () => {
+      describe('op code', () => {
+        describe('with value [170,1,1,0,91,0,0,93,0,0,2,195,0,0,0,0,1,0,0,108]', () => {
+          it('sets the value to detected, 91cm, 707s', async () => {
+            expect(
+              await testParseStateCalled(state, {
+                op: {
+                  command: [
+                    [
+                      170, 1, 1, 0, 91, 0, 0, 93, 0, 0, 2, 195, 0, 0, 0, 0, 1,
+                      0, 0, 108,
+                    ],
+                  ],
+                },
+              }),
+            ).toStrictEqual({
+              detected: true,
+              distance: {
+                value: 91,
+                unit: 'cm',
+              },
+              duration: {
+                value: 707,
+                unit: 's',
+              },
+              type: 'mmWave',
+            });
+          });
+        });
+        describe('with value [170,1,0,2,1,1,0,93,0,0,0,10,0,0,0,0,1,0,0,108]', () => {
+          it('sets the value to not detected, 513cm, 10s', async () => {
+            expect(
+              await testParseStateCalled(state, {
+                op: {
+                  command: [
+                    [
+                      170, 1, 0, 2, 1, 1, 0, 93, 0, 0, 0, 10, 0, 0, 0, 0, 1, 0,
+                      0, 108,
+                    ],
+                  ],
+                },
+              }),
+            ).toStrictEqual({
+              detected: false,
+              distance: {
+                value: 513,
+                unit: 'cm',
+              },
+              duration: {
+                value: 10,
+                unit: 's',
+              },
+              type: 'mmWave',
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+describe('BiologicalPresenceState', () => {
+  let state: BiologicalPresenceState;
+
+  describe('parse', () => {
+    beforeEach(() => {
+      state = new BiologicalPresenceState(deviceModel);
+    });
+
+    describe('when passed', () => {
+      describe('an invalid', () => {
+        describe('state argument', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {}),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('op codes with different OpType', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {
+                op: { command: [[0x02, 0x02, 0x01]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('op codes with different identifier', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {
+                op: { command: [[OpType.REPORT, 0x02, 0x01]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+      });
+    });
+    describe('a valid', () => {
+      describe('op code', () => {
+        describe('with value [170,1,1,0,91,0,0,93,0,0,2,195,0,0,0,0,1,0,0,108]', () => {
+          it('sets the value to not detected, 93cm, 707s', async () => {
+            expect(
+              await testParseStateCalled(state, {
+                op: {
+                  command: [
+                    [
+                      170, 1, 1, 0, 91, 0, 0, 93, 0, 0, 2, 195, 0, 0, 0, 0, 1,
+                      0, 0, 108,
+                    ],
+                  ],
+                },
+              }),
+            ).toStrictEqual({
+              detected: false,
+              distance: {
+                value: 93,
+                unit: 'cm',
+              },
+              duration: {
+                value: 707,
+                unit: 's',
+              },
+              type: 'biological',
+            });
+          });
+        });
+        describe('with value [170,1,1,0,163,1,1,57,0,0,2,135,0,0,0,0,1,0,0,181]', () => {
+          it('sets the value to detected, 317cm, 647s', async () => {
+            expect(
+              await testParseStateCalled(state, {
+                op: {
+                  command: [
+                    [
+                      170, 1, 1, 0, 163, 1, 1, 57, 0, 0, 2, 135, 0, 0, 0, 0, 1,
+                      0, 0, 181,
+                    ],
+                  ],
+                },
+              }),
+            ).toStrictEqual({
+              detected: true,
+              distance: {
+                value: 313,
+                unit: 'cm',
+              },
+              duration: {
+                value: 647,
+                unit: 's',
+              },
+              type: 'biological',
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+describe('EnablePresenceState', () => {
+  let state: EnablePresenceState;
+  describe('parse', () => {
+    describe('when passed', () => {
+      describe('an invalid', () => {
+        beforeEach(() => {
+          state = new EnablePresenceState(deviceModel);
+        });
+        describe('state argument', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {}),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('op codes with different OpType', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {
+                op: { command: [[0x02, 0x02, 0x01]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('op codes with different identifier', () => {
+          it('does not update the value', async () => {
+            expect(
+              await testParseStateNotCalled(state, {
+                op: { command: [[OpType.REPORT, 0x02, 0x01]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+      });
+    });
+    describe('a valid', () => {
+      describe('op code', () => {
+        describe('with value [170,31,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,181]', () => {
+          let actualState: EnablePresenceFlags;
+          beforeAll(() => {
+            state = new EnablePresenceState(deviceModel);
+          });
+          beforeEach(async () => {
+            actualState = await testParseStateCalled(state, {
+              op: {
+                command: [
+                  [
+                    170, 31, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    181,
+                  ],
+                ],
+              },
+            });
+          });
+          it('sets the value to true, true', () => {
+            expect(actualState).toStrictEqual({
+              mmWaveEnabled: true,
+              biologicalEnabled: true,
+            });
+          });
+        });
+        describe('setState with biologicalEnabled missing', () => {
+          it('does return a command', async () => {
+            expect(state.value).toStrictEqual({
+              mmWaveEnabled: true,
+              biologicalEnabled: true,
+            });
+            expect(
+              await testSetStateCalled(state, { mmWaveEnabled: false }),
+            ).toStrictEqual({
+              command: 'multiSync',
+              commandId: expect.any(String),
+              data: {
+                command: [asOpCode(OpType.COMMAND, 31, 1, 0)],
+              },
+            });
+          });
+        });
+        describe('setState with mmWaveEnabled missing', () => {
+          it('does return a command', async () => {
+            expect(state.value).toStrictEqual({
+              mmWaveEnabled: true,
+              biologicalEnabled: true,
+            });
+            expect(
+              await testSetStateCalled(state, { biologicalEnabled: false }),
+            ).toMatchObject({
+              command: 'multiSync',
+              commandId: expect.any(String),
+              data: {
+                command: [asOpCode(OpType.COMMAND, 31, 0, 1)],
+              },
+            });
+          });
+        });
+      });
+    });
+  });
+  describe('setState', () => {
+    beforeEach(() => {
+      state = new EnablePresenceState(deviceModel);
+    });
+    describe('when nextState is', () => {
+      describe('{}', () => {
+        it('does not return a command', async () => {
+          expect(await testSetStateNotCalled(state, {})).not.toHaveBeenCalled();
+        });
+      });
+      describe('missing mmWaveEnabled and', () => {
+        describe('current mmWaveEnabled is', () => {
+          describe('undefined', () => {
+            it('does not return a command', async () => {
+              expect(
+                await testSetStateNotCalled(state, { biologicalEnabled: true }),
+              ).not.toHaveBeenCalled();
+            });
+          });
+        });
+      });
+      describe('missing biologicalEnabled and', () => {
+        describe('current biologicalEnabled is', () => {
+          describe('undefined', () => {
+            it('does not return a command', async () => {
+              expect(
+                await testSetStateNotCalled(state, { mmWaveEnabled: true }),
+              ).not.toHaveBeenCalled();
+            });
+          });
+        });
+      });
+
+      describe('not missing mmWaveEnabled and not missing biologicalEnabled', () => {
+        it.each([
+          [
+            { mmWaveEnabled: true, biologicalEnabled: true },
+            {
+              data: {
+                command: [asOpCode(OpType.COMMAND, 31, 1, 1)],
+              },
+            },
+          ],
+          [
+            { mmWaveEnabled: false, biologicalEnabled: true },
+            {
+              data: {
+                command: [asOpCode(OpType.COMMAND, 31, 1, 0)],
+              },
+            },
+          ],
+          [
+            { mmWaveEnabled: true, biologicalEnabled: false },
+            {
+              data: {
+                command: [asOpCode(OpType.COMMAND, 31, 0, 1)],
+              },
+            },
+          ],
+          [
+            { mmWaveEnabled: false, biologicalEnabled: false },
+            {
+              data: {
+                command: [asOpCode(OpType.COMMAND, 31, 0, 0)],
+              },
+            },
+          ],
+        ])(
+          '%p returns as the command %p',
+          async (nextState, expectedCommand) => {
+            expect(state.value).toStrictEqual({});
+            expect(await testSetStateCalled(state, nextState)).toMatchObject(
+              expectedCommand,
+            );
+          },
+        );
+      });
+    });
+  });
+});
---
+++ lib/domain/devices/states/unknown.state.spec.ts
@@ -0,0 +1,138 @@
+import { Subscription } from 'rxjs';
+import { DeviceModel } from '../devices.model';
+import { UnknownState } from './unknown.state';
+import { OpType } from '../../../common';
+import { Version } from '../version.info';
+import { GoveeDeviceStatus } from '../../../data';
+import {
+  testParseStateCalled,
+  testParseStateNotCalled,
+} from '../../../common/test-utils';
+
+describe('UnknownState', () => {
+  const deviceModel: DeviceModel = new DeviceModel({
+    id: '01:02:03:04:05:06:07:08',
+    ic: 0,
+    name: 'Test device',
+    category: 'Generic',
+    categoryGroup: 'Device',
+    model: 'H7567',
+    modelName: 'Some kind of device',
+    goodsType: 30,
+    pactType: 10,
+    pactCode: 4,
+    version: new Version('1.0.0', '2.0.0'),
+    state: {},
+  });
+  const unknownState25 = new UnknownState(deviceModel, OpType.REPORT, 0x19);
+  const unknownState5_3 = new UnknownState(
+    deviceModel,
+    OpType.REPORT,
+    0x05,
+    0x03,
+  );
+  describe('parse', () => {
+    let subscription: Subscription | undefined;
+    afterEach(() => {
+      subscription?.unsubscribe();
+    });
+    describe('when passed', () => {
+      describe('state', () => {
+        it('value is not updated', async () => {
+          const forState = async (state: UnknownState) => {
+            return await testParseStateNotCalled(state, { state: {} });
+          };
+          expect(await forState(unknownState25)).not.toHaveBeenCalled();
+          subscription?.unsubscribe();
+          expect(await forState(unknownState5_3)).not.toHaveBeenCalled();
+        });
+      });
+      describe('opCode', () => {
+        describe('with no op-code', () => {
+          it('value is not updated', async () => {
+            const forState = async (state: UnknownState) => {
+              return await testParseStateNotCalled(state, { op: {} });
+            };
+            expect(await forState(unknownState25)).not.toHaveBeenCalled();
+            subscription?.unsubscribe();
+            expect(await forState(unknownState5_3)).not.toHaveBeenCalled();
+          });
+        });
+        describe('with wrong op-type', () => {
+          it('value is not updated', async () => {
+            const forState = async (
+              state: UnknownState,
+              status: Partial<GoveeDeviceStatus>,
+            ) => {
+              return await testParseStateNotCalled(state, status);
+            };
+            expect(
+              await forState(unknownState25, {
+                op: { command: [[100, 10, 10, 10, 10]] },
+              }),
+            ).not.toHaveBeenCalled();
+            subscription?.unsubscribe();
+            expect(
+              await forState(unknownState5_3, {
+                op: { command: [[100, 10, 10, 10, 10]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('with wrong identifier', () => {
+          it('value is not updated', async () => {
+            const forState = async (
+              state: UnknownState,
+              status: Partial<GoveeDeviceStatus>,
+            ) => {
+              return await testParseStateNotCalled(state, status);
+            };
+            expect(
+              await forState(unknownState25, {
+                op: { command: [[OpType.REPORT, 10, 10, 10, 10]] },
+              }),
+            ).not.toHaveBeenCalled();
+            subscription?.unsubscribe();
+            expect(
+              await forState(unknownState5_3, {
+                op: { command: [[OpType.REPORT, 10, 10, 10, 10]] },
+              }),
+            ).not.toHaveBeenCalled();
+          });
+        });
+        describe('with correct identifier', () => {
+          it('value is updated', async () => {
+            const forState = async (
+              state: UnknownState,
+              opType: number,
+              identifier: number[],
+              stateCodes: number[],
+            ) => {
+              return await testParseStateCalled(state, {
+                op: { command: [[opType, ...identifier, ...stateCodes]] },
+              });
+            };
+            let expectedCodes = [0x01, 0x10, 0x05];
+            expect(
+              await forState(
+                unknownState25,
+                OpType.REPORT,
+                [25],
+                expectedCodes,
+              ),
+            ).toEqual({ codes: expectedCodes });
+            expectedCodes = [0x19, 0x05];
+            expect(
+              await forState(
+                unknownState5_3,
+                OpType.REPORT,
+                [5, 3],
+                expectedCodes,
+              ),
+            ).toEqual({ codes: expectedCodes });
+          });
+        });
+      });
+    });
+  });
+});
