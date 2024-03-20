00df286ec4c7fc8e28e407d44c38bbb112ca9dd2
Teagan42 <that@teagantotally.rocks>
2024-03-20 05:53:47 -0600

    lint

--- lib/data/ble/decoder/assets/H5179_json.ts
+++ lib/data/ble/decoder/assets/H5179_json.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-useless-escape */
 export const spec: string = `
 #include "common_props.h"
 
+/* trunk-ignore(eslint/no-useless-escape) */
 const char* _H5179_json = "{\"brand\":\"Govee\",\"model\":\"Thermo-Hygrometer\",\"model_id\":\"H5179\",\"tag\":\"0103\",\"condition\":[\"name\",\"index\",0,\"Govee_H5179\",\"&\",\"manufacturerdata\",\"=\",22,\"index\",0,\"0188ec\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",12,4,true,true],\"post_proc\":[\"/\",100]},\"hum\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",16,4,true,false],\"post_proc\":[\"/\",100]},\"batt\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,2,false,false]}}}";
 /* R""""(
 {
