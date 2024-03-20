7eb8a7ad4a6ced1df8f0086495b1644423412203
Teagan42 <that@teagantotally.rocks>
2024-03-20 04:26:08 -0600

    Fix url and import

--- lib/data/ble/decoder/decoder.config.ts
+++ lib/data/ble/decoder/decoder.config.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 const deviceJsonUrl = (model: string) =>
-  `https://raw.githubusercontent.com/theengs/decoder/development/src/devices/${model}_json.h`;
+  `https://raw.githubusercontent.com/IoTManagerProject/IoTManager/ver4stable/lib/decoder/src/devices/${model}_json.h`;
 const commonPropertiesUrl =
-  'https://raw.githubusercontent.com/theengs/decoder/development/src/devices/common_props.h';
+  'https://raw.githubusercontent.com/IoTManagerProject/IoTManager/ver4stable/lib/decoder/src/devices/common_props.h';
 
 export const DecoderConfig = registerAs('Configuration.Decoder', () => ({
   deviceJsonUrl,
