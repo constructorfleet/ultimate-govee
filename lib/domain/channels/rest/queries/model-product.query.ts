3d02152d633db611593b1fdab60293e5f30731fc
Teagan Glenn <that@teagantotally.rocks>
2024-06-08 20:43:04 -0600

    chore: Miscellaneous fixes (#197)
    * Package lock
    
    * Change log levels
    
    * Chagne to onModuleInit

--- lib/domain/channels/rest/queries/model-product.query.ts
+++ lib/domain/channels/rest/queries/model-product.query.ts
@@ -6,7 +6,5 @@ export class ModelProductQuery implements Labelled {
   label = () => `Product Info For ${this.device.model}`;
 
   private readonly logger = new Logger(ModelProductQuery.name);
-  constructor(readonly device: GoveeDevice) {
-    this.logger.log(`${this.device.model}`);
-  }
+  constructor(readonly device: GoveeDevice) {}
 }
