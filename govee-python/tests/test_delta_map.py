115388ac19e4182cc84e10ac9226723ca37591b9
Teagan glenn <that@teagantotally.rocks>
2025-11-08 09:02:19 -0700

    test(common): add tests for DeltaMap behaviour

---
+++ govee-python/tests/test_delta_map.py
@@ -0,0 +1,29 @@
+from govee.common.delta_map import DeltaMap
+from govee.common.delta_types import MapDelta
+
+
+def test_delta_map_add_mod_del():
+    dm = DeltaMap()
+    seen = []
+
+    def cb(d):
+        seen.append(d)
+
+    dm.delta_subject.subscribe(cb)
+    dm["a"] = 1
+    assert seen and seen[-1].all["a"] == 1
+    dm["a"] = 2
+    assert seen[-1].all["a"] == 2
+    del dm["a"]
+    assert "a" not in dm.get_delta().all
+
+
+def test_pause_resume():
+    dm = DeltaMap()
+    seen = []
+    dm.delta_subject.subscribe(lambda d: seen.append(d))
+    dm.pause()
+    dm["x"] = 10
+    assert not seen
+    dm.resume()
+    assert seen
