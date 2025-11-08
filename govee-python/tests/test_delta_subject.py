bfff84f27f55ab94b76ce5cd3a97605c7359c510
Teagan glenn <that@teagantotally.rocks>
2025-11-08 08:48:17 -0700

    test(common): add DeltaSubject unit test

---
+++ govee-python/tests/test_delta_subject.py
@@ -0,0 +1,15 @@
+from govee.common.observables import DeltaSubject
+from govee.common.delta_types import MapDelta
+
+
+def test_delta_subject_forwards_delta():
+    ds = DeltaSubject()
+    seen = []
+
+    def cb(d):
+        seen.append(d)
+
+    ds.subscribe(cb)
+    delta = MapDelta(all={'a':1}, added={'a':1}, modified={}, deleted={})
+    ds.next_delta(delta)
+    assert seen and seen[0].all['a'] == 1
