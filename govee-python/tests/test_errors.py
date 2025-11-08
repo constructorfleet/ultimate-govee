908960b28cf82ac33fb27b612e5e4e9e4f4f692b
Teagan glenn <that@teagantotally.rocks>
2025-11-08 07:56:12 -0700

    test(errors): add basic GoveeError test

--- govee-python/tests/test_errors.py
+++ govee-python/tests/test_errors.py
@@ -1,14 +1,7 @@
-import pytest
-
-from govee.errors import GoveeError, GoveeApiError, GoveeCommunityApiError
+from govee import GoveeError
 
 
 def test_govee_error_message():
-    e = GoveeError("boom")
-    assert "boom" in str(e)
-
-
-def test_api_errors():
-    assert "Govee" in str(GoveeApiError("a"))
-    assert "GoveeCommunity" in str(GoveeCommunityApiError("b"))
+    e = GoveeError('something went wrong')
+    assert 'Govee' in str(e)
 
