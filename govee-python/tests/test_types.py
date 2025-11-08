15b33ce2266b22bd9399806dbff3171f69ddfbce
Teagan glenn <that@teagantotally.rocks>
2025-11-08 07:56:31 -0700

    test(types): add tests for types.resolve_label and dataclasses

--- govee-python/tests/test_types.py
+++ govee-python/tests/test_types.py
@@ -1,14 +1,23 @@
-from govee.types import Credentials, resolve_label
+from govee.types import resolve_label, Credentials, DeviceCommandAddresses
+
+
+def test_resolve_label_with_string():
+    assert resolve_label('simple') == 'simple'
+
+
+def test_resolve_label_with_callable():
+    assert resolve_label(lambda: 'computed') == 'computed'
 
 
 def test_credentials_dataclass():
-    c = Credentials(username="u", password="p", client_id="cid")
-    assert c.username == "u"
-    assert c.password == "p"
-    assert c.client_id == "cid"
+    cred = Credentials(username='u', password='p', client_id='cid')
+    assert cred.username == 'u'
+    assert cred.password == 'p'
+    assert cred.client_id == 'cid'
 
 
-def test_resolve_label():
-    assert resolve_label("simple") == "simple"
-    assert resolve_label(lambda: "computed") == "computed"
+def test_device_command_addresses_defaults():
+    d = DeviceCommandAddresses()
+    assert d.iot_topic is None
+    assert d.ble_address is None
 
