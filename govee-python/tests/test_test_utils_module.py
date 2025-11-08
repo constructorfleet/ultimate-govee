7a121242b90f2bfd627796665215e59939379d72
Teagan glenn <that@teagantotally.rocks>
2025-11-08 07:56:49 -0700

    test(test_utils): add tests for DummyState behavior

---
+++ govee-python/tests/test_test_utils_module.py
@@ -0,0 +1,14 @@
+from govee.test_utils import DummyState
+
+
+def test_dummy_state_parse_and_set_state():
+    d = DummyState()
+    d.parse({'x': 1})
+    assert d.parse_calls == [{'x': 1}]
+
+    # set_state should record and optionally emit commands
+    cmds = d.set_state({'emit': 'on'})
+    assert d.set_state_calls == [{'emit': 'on'}]
+    assert cmds == [{'cmd': 'on'}]
+    assert d.command_bus == [{'cmd': 'on'}]
+
