0cd3b08d5d456e71515180b6a1387c5043803488
Teagan glenn <that@teagantotally.rocks>
2025-11-08 07:56:08 -0700

    test(core): add tests for FixedLengthStack

--- govee-python/tests/test_fixed_length_stack.py
+++ govee-python/tests/test_fixed_length_stack.py
@@ -1,25 +1,20 @@
-from govee.fixed_length_stack import FixedLengthStack
+from govee import FixedLengthStack
 
 
-def test_fixed_length_stack_basic():
-    s = FixedLengthStack[int](2)
+def test_stack_basic():
+    s = FixedLengthStack[int](3)
     assert s.size() == 0
     s.enstack(1)
     s.enstack(2)
-    assert s.size() == 2
-    assert s.peek() == 2
     s.enstack(3)
-    # oldest (1) should be dropped
+    assert s.size() == 3
+    assert s.peek() == 3
+
+    s.enstack(4)
+    # oldest element (1) should be dropped
+    assert s.size() == 3
+    assert s.peek_all() == [4, 3, 2]
+
+    assert s.destack() == 4
     assert s.size() == 2
-    assert s.peek_all() == [3, 2]
-    assert s.destack() == 3
-    assert s.destack() == 2
-    assert s.destack() is None
-
-
-def test_fixed_length_stack_clear():
-    s = FixedLengthStack[str](1)
-    s.enstack("a")
-    s.clear()
-    assert s.size() == 0
 
