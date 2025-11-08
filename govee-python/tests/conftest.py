b3f16b8afcf33fcc60aa4bbdf2d873344f961a32
Teagan glenn <that@teagantotally.rocks>
2025-11-08 13:32:25 -0700

    chore(tests): add conftest to put src on PYTHONPATH for pytest

---
+++ govee-python/tests/conftest.py
@@ -0,0 +1,6 @@
+# conftest for pytest: ensure package src directory is on sys.path
+import sys, os
+_here = os.path.dirname(os.path.abspath(__file__))
+_root = os.path.abspath(os.path.join(_here, '..', 'src'))
+if _root not in sys.path:
+    sys.path.insert(0, _root)
