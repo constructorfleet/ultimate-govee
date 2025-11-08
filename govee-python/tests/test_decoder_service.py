7637d89a93abe7de370965a1c97ce4c0db1dc4c7
Teagan glenn <that@teagantotally.rocks>
2025-11-08 12:40:57 -0700

    test(ble): add decoder service unit tests for manufacturer_data parsing

---
+++ govee-python/tests/test_decoder_service.py
@@ -0,0 +1,43 @@
+import asyncio
+from govee.data.ble.decoder_service import DecoderService
+
+
+def test_decode_device_without_name_returns_none():
+    svc = DecoderService()
+    peripheral = {'id': '1', 'address': 'AA:BB:CC'}
+    res = asyncio.get_event_loop().run_until_complete(svc.decode_device(peripheral))
+    assert res is None
+
+
+def test_decode_device_parses_bytes_manufacturer_data():
+    svc = DecoderService()
+    peripheral = {
+        'id': '1',
+        'address': 'AA:BB:CC:DD:EE:FF',
+        'advertisement': {
+            'localName': 'Govee',
+            'manufacturer_data': b'H6112|AA:BB:CC:DD:EE:FF',
+        },
+    }
+    res = asyncio.get_event_loop().run_until_complete(svc.decode_device(peripheral))
+    assert res is not None
+    assert res.get('model') == 'H6112'
+    assert res.get('mac') == 'AA:BB:CC:DD:EE:FF'
+
+
+def test_decode_device_parses_hex_string_manufacturer_data():
+    svc = DecoderService()
+    raw = b'H6112|AA:BB:CC:DD:EE:FF'
+    hexstr = raw.hex()
+    peripheral = {
+        'id': '1',
+        'address': 'AA:BB:CC:DD:EE:FF',
+        'advertisement': {
+            'localName': 'Govee',
+            'manufacturer_data': hexstr,
+        },
+    }
+    res = asyncio.get_event_loop().run_until_complete(svc.decode_device(peripheral))
+    assert res is not None
+    assert res.get('model') == 'H6112'
+    assert res.get('mac') == 'AA:BB:CC:DD:EE:FF'
