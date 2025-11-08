2b7d7f7f6e035e161ccb121c91227fc2613b837d
Teagan glenn <that@teagantotally.rocks>
2025-11-08 07:56:03 -0700

    test(bitflags): add unit tests for bitflags operations

--- govee-python/tests/test_bitflags.py
+++ govee-python/tests/test_bitflags.py
@@ -1,17 +1,22 @@
-from govee.bitflags import create_bitflags_enum
+from govee import create_bitflags_enum
 
 
-def test_bitflags_basic():
-    enums = create_bitflags_enum(["A", "B", "C"]) 
-    assert enums.keys == ["A", "B", "C"]
-    a = enums.A
-    b = enums.B
-    c = enums.C
+def test_create_bitflags_and_operations():
+    flags = create_bitflags_enum(["A", "B", "C"])
+    a = flags.A
+    b = flags.B
+    c = flags.C
+
     assert a.value == 1
     assert b.value == 2
     assert c.value == 4
-    assert a.or_(b).value == 3
-    assert enums.union([a, c]).value == 5
-    assert a.has_flag(a)
-    assert not a.has_flag(b)
+
+    union = a.or_(b)
+    assert union.value == 3
+    assert union.has_flag(a)
+    assert union.has_flag(b)
+    assert not union.has_flag(c)
+
+    combined = flags.union([a, c])
+    assert combined.value == 5
 
