45e47fadb971c1ad7ca06d9cb14175e566d18c09
Teagan glenn <that@teagantotally.rocks>
2025-11-09 00:26:06 -0700

    fix(tests): use asyncio.run in decoder service tests to avoid 'no current event loop' warning

--- govee-python/tests/test_decoder_service.py
+++ govee-python/tests/test_decoder_service.py
@@ -5,7 +5,7 @@ from govee.data.ble.decoder_service import DecoderService
 def test_decode_device_without_name_returns_none():
     svc = DecoderService()
     peripheral = {'id': '1', 'address': 'AA:BB:CC'}
-    res = asyncio.get_event_loop().run_until_complete(svc.decode_device(peripheral))
+    res = asyncio.run(svc.decode_device(peripheral))
     assert res is None
 
 
@@ -19,7 +19,7 @@ def test_decode_device_parses_bytes_manufacturer_data():
             'manufacturer_data': b'H6112|AA:BB:CC:DD:EE:FF',
         },
     }
-    res = asyncio.get_event_loop().run_until_complete(svc.decode_device(peripheral))
+    res = asyncio.run(svc.decode_device(peripheral))
     assert res is not None
     assert res.get('model') == 'H6112'
     assert res.get('mac') == 'AA:BB:CC:DD:EE:FF'
@@ -37,7 +37,7 @@ def test_decode_device_parses_hex_string_manufacturer_data():
             'manufacturer_data': hexstr,
         },
     }
-    res = asyncio.get_event_loop().run_until_complete(svc.decode_device(peripheral))
+    res = asyncio.run(svc.decode_device(peripheral))
     assert res is not None
     assert res.get('model') == 'H6112'
     assert res.get('mac') == 'AA:BB:CC:DD:EE:FF'
