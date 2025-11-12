e2bf6428ed4c22de333c1e7fb38493c966c5fa12
Teagan glenn <that@teagantotally.rocks>
2025-11-11 18:27:08 -0700

    feat(openapi): add IoTCredentialModel TypedDict and parser

---
+++ govee-python/src/govee/data/openapi/models.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TypedDict, Optional
+
+
+class IoTCredentialModel(TypedDict):
+    certificate: str
+    privateKey: str
+    endpoint: str
+    accountId: str
+    clientId: str
+    topic: str
+
+
+def parse_iot_credentials(obj: dict) -> IoTCredentialModel:
+    data = obj.get('data', obj)
+    return {
+        'certificate': data.get('certificate', ''),
+        'privateKey': data.get('privateKey', ''),
+        'endpoint': data.get('endpoint', ''),
+        'accountId': data.get('accountId', ''),
+        'clientId': data.get('clientId', ''),
+        'topic': data.get('topic', ''),
+    }
+
